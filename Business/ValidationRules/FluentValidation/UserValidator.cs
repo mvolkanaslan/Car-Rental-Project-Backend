29d9806b0467cb0221fe1072e4fc2fd19736a292
mvolkanaslan <mvolkanaslan@yandex.com>
2021-02-22 19:25:30 +0300

    Validation User password using specific regex rule.

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Business.ValidationRules.FluentValidation
 {
@@ -12,7 +13,18 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(u => u.Email).NotEmpty();
             RuleFor(u => u.Email).EmailAddress();
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(8);
+            RuleFor(u => u.Password).Must(ContainsThese).WithMessage("Min 8 characters, must has number and letter");
 
+        }
+
+        private bool ContainsThese(string arg)
+        {
+            //MİN 8 karakter ve sayı ve harf içermeli
+            return Regex.IsMatch(arg, @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
         }
     }
 }
